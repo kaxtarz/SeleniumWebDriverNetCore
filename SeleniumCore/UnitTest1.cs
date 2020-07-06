7a0abc56e86101f28ad1912c2235fe67f61af3d9
kesa lee <kaxtarz@hotmail.com>
2020-07-05 22:40:47 -0500

    * .gitignore: Initial commit for Selenium WebDriver Net Core
    * SeleniumCore.sln:
    * UnitTest1.cs:
    * chromedriver:
    * SeleniumCore.csproj:

---
+++ SeleniumCore/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class HomepageFeature
+    {
+        [TestMethod]
+        public void ShouldBeAbleToLogin()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+        }
+    }
+}
